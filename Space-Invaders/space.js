2736a759084d061e8d2d1246ee079caa07015a72
Handy <dorceus.handy@gmail.com>
2025-09-30 15:46:37 -0400

    added black background to insert game

---
+++ Space-Invaders/space.js
@@ -0,0 +1,29 @@
+//board
+let tileSize = 32;
+let rows = 16;
+let columns = 16;
+
+let board;
+let boardWidth = tileSize * columns; // 32 * 16
+let boardHeight = tileSize * rows; // 32 * 16
+let context;
+
+//ship
+let shipWidth = tileSize*2;
+let shipHeight = tileSize;
+let shipX = tileSize * columns/2 - tileSize;
+let shipY = tileSize * rows - tileSize*2
+
+let ship = {
+    x :shipX,
+    y: shipY,
+    width : shipWidth,
+    height : shipHeight
+}
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.width = boardWidth;
+    board.height = boardHeight;
+    content = board.getContext("2d") //used for drawing on the board
+}
