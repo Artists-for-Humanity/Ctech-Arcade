110313e5006f64a26f5ac5a188487d1e8c4b8509
William <wfchen30@gmail.com>
2025-10-01 17:49:28 -0400

    properly animated the block, and added random lane

--- 2d_Subway_Surfer/script.js
+++ 2d_Subway_Surfer/script.js
@@ -22,3 +22,28 @@ document.addEventListener("keydown", event => {
     if(event.key==="ArrowLeft"){moveLeft();}
     if(event.key==="ArrowRight"){moveRight();}
 });
+
+var block = document.getElementById("barrier_1");
+var counter = 0;
+barrier_1.addEventListener('animationiteration', () => {
+    var random = Math.floor(Math.random() * 3);
+    left = random * 200;
+    block.style.left = left + "px";
+    counter++;
+});
+
+setInterval(function(){
+    var playerLeft = 
+    parseInt(window.getComputedStyle(player)
+    .getPropertyValue("left"));
+    var blockLeft = 
+    parseInt(window.getComputedStyle(block)
+    .getPropertyValue("left"));
+    var blockTop = 
+    parseInt(window.getComputedStyle(block)
+    .getPropertyValue("top"));
+    if(playerLeft == blockLeft && blockTop<900&& blockTop>600){
+        alert("Game Over. Score: " + counter)
+        block.style.animation = "none";
+    }
+}, 1);
