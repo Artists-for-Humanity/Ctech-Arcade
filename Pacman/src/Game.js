c8655383555ffee2b6186e6f28186b5d4679ee55
William <wfchen30@gmail.com>
2025-10-29 17:30:29 -0400

    added Pacman movement, and wall colliders

--- Pacman/src/Game.js
+++ Pacman/src/Game.js
@@ -1,7 +1,8 @@
 import TileMap from "./TileMap.js";
 
 const tileSize = 32;
-const velocity = 1;
+const velocity = 2;
+
 const canvas = document.getElementById('gameCanvas')
 const ctx = canvas.getContext('2d');
 const tileMap = new TileMap(tileSize);
@@ -9,7 +10,7 @@ const pacman = tileMap.getPacman(velocity);
 
 function gameLoop(){
     tileMap.draw(ctx);
-    
+    pacman.draw(ctx);
 }
 
 tileMap.setCanvasSize(canvas);
--- Pacman/src/TileMap.js
+++ Pacman/src/TileMap.js
@@ -1,4 +1,5 @@
 import Pacman from "./Pacman.js";
+import MovingDirection from "./MovingDirection.js";
 
 export default class TileMap{
     constructor(tileSize) {
@@ -38,22 +39,39 @@ export default class TileMap{
                     let tile = this.map[row][column];
                     if(tile===1){
                         this.#drawblueWall(ctx, column, row, this.tileSize);
-                    } else if (tile === 0) {
+                    } else if (tile===0) {
                         this.#drawDot(ctx, column, row, this.tileSize);
                     }
-                    // ctx.strokeStyle ="yellow";
-                    // ctx.strokeRect(column* this.tileSize, row * this.tileSize, this.tileSize, this.tileSize )
+                    ctx.strokeStyle ="yellow";
+                    ctx.strokeRect(
+                        column * this.tileSize, 
+                        row * this.tileSize, 
+                        this.tileSize, 
+                        this.tileSize 
+                    );
                 }
 
         }
     }    
 
     #drawDot(ctx,column,row,size) {
-        ctx.drawImage(this.yellowDot, column * this.tileSize, row * this.tileSize, size,size   )
+        ctx.drawImage(
+            this.yellowDot, 
+            column * this.tileSize, 
+            row * this.tileSize, 
+            size,
+            size
+        );
     }
-
+  
     #drawblueWall(ctx, column, row, size) {
-        ctx.drawImage(this.blueWall,column*this.tileSize,row * this.tileSize, size,size)
+        ctx.drawImage(
+            this.blueWall,
+            column*this.tileSize,
+            row * this.tileSize, 
+            size,
+            size
+        );
 
     }
 
@@ -62,7 +80,6 @@ export default class TileMap{
             for(let column = 0; column < this.map[row].length; column++){
                 let tile = this.map[row][column];
                 if(tile === 4){
-                    this.map[row][column] = 0;
                     return new Pacman(column * this.tileSize, row*this.tileSize, this.tileSize, velocity, this);
                 }
             }
@@ -72,5 +89,44 @@ export default class TileMap{
     setCanvasSize(canvas) {
         canvas.width = this.map[0].length * this.tileSize;
         canvas.height = this.map.length * this.tileSize;
+    }
+
+    didCollideWithEnvironment(x,y,direction){
+        if(Number.isInteger(x/this.tileSize) &&
+            Number.isInteger(y/this.tileSize)
+        ) {
+            let column = 0;
+            let row = 0;
+            let nextColumn = 0;
+            let nextRow = 0;
+
+            switch(direction){
+                case MovingDirection.right:
+                    nextColumn = x + this.tileSize;
+                    column = nextColumn /this.tileSize;
+                    row = y / this.tileSize;
+                    break;
+                case MovingDirection.left:
+                    nextColumn = x - this.tileSize;
+                    column = nextColumn /this.tileSize;
+                    row = y / this.tileSize;
+                    break;
+                case MovingDirection.up:
+                    nextRow = y - this.tileSize;
+                    row = nextRow /this.tileSize;
+                    column = x / this.tileSize;
+                    break;
+                case MovingDirection.down:
+                    nextRow = y + this.tileSize;
+                    row = nextRow / this.tileSize;
+                    column = x / this.tileSize;
+                    break;
+            }
+            const tile = this.map[row][column];
+            if (tile === 1) {
+                return true;
+            }
+        }
+        return false;
     }
 }
